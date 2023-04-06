51ddd42acc6c1dba2f56d96a251c9e37476887f1
andreaszico <zicoaritonang@gmail.com>
2023-04-06 13:03:45 +0700

    fixing

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -174,22 +174,16 @@ class Role {
   });
   late final int id;
   late final String name;
-  late final String createdAt;
-  late final String updatedAt;
 
   Role.fromJson(Map<String, dynamic> json){
     id = json['id'];
     name = json['name'];
-    createdAt = json['created_at'];
-    updatedAt = json['updated_at'];
   }
 
 Map<String, dynamic> toJson() {
     final _data = <String, dynamic>{};
     _data['id'] = id;
     _data['name'] = name;
-    _data['created_at'] = createdAt;
-    _data['updated_at'] = updatedAt;
     return _data;
   }
 
